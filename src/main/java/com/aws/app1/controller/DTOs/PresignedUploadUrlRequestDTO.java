b4d83effa7dc0b7ebc9c4b3217e89e48daf9d636
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-06 12:18:57 -0300

    Added params

--- src/main/java/com/aws/app1/controller/DTOs/PresignedUploadUrlRequestDTO.java
+++ src/main/java/com/aws/app1/controller/DTOs/PresignedUploadUrlRequestDTO.java
@@ -1,4 +1,22 @@
 package com.aws.app1.controller.DTOs;
 
-public class PresignedUploadUrlRequestDTO {
-}
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record PresignedUploadUrlRequestDTO(
+        @NotBlank
+        String bucketName,
+
+        @NotBlank
+        String key,
+
+        @NotNull
+        Long userId,
+
+        @NotBlank
+        String contentType,
+
+        @Schema(defaultValue = "3600")
+        long expirationSeconds
+) {}
